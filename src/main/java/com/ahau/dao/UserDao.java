8309a3fcd90b61bb44d2d4532b1ccdafab45f04a
volx <hastingsvol@163.com>
2023-02-07 03:48:53 +0800

    删除登录注册后端代码

--- src/main/java/com/ahau/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.ahau.dao;
-
-import com.ahau.domain.User;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserDao extends BaseMapper<User> {
-}
