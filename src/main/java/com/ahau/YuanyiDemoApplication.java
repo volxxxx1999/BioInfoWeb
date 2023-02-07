8224c4844016a0e69d45f9d0d81b1f1ce4c3cbb1
volx <hastingsvol@163.com>
2023-02-07 16:57:47 +0800

    注释掉pom的数据库相关依赖，删除了用户登陆注册相关代码

--- src/main/java/com/ahau/YuanyiDemoApplication.java
+++ src/main/java/com/ahau/YuanyiDemoApplication.java
@@ -1,11 +1,11 @@
 package com.ahau;
 
-import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@MapperScan(value = "com.ahau.dao")
+
+
 @SpringBootApplication()
 public class YuanyiDemoApplication {
 
