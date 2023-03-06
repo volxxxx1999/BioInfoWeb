0f3753dc8f407326e6d1f3788cb2dfdeea8a2abb
volx <hastingsvol@163.com>
2023-03-06 16:33:33 +0800

    首页demos的后端

--- src/main/java/com/ahau/controller/ResultController.java
+++ src/main/java/com/ahau/controller/ResultController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 
 @RestController
@@ -37,6 +38,33 @@ public class ResultController {
             case "Telo":
                 return new Result(Code.TELO_SEARCH_OK, "success");
             case "Centro":
+                return new Result(Code.CENTRO_SEARCH_OK, "success");
+        }
+        return null;
+    }
+
+
+    /**
+     * @Description: 根据Demo号返回不同的结果
+     * @Param: demoNum
+     * @Return: Result
+     */
+    @GetMapping("/goDemo/{demoNum}")
+    public Result getInstance(HttpServletRequest request, @PathVariable String demoNum) {
+        System.out.println("===》ResultController:getInstance");
+        // 1. 设置taskID
+        String taskID = "instance/" + demoNum + "/";
+        System.out.println("======》taskID:\t" + taskID);
+        HttpSession session = request.getSession();
+        session.setAttribute("TaskID", taskID);
+        switch (demoNum) {
+            case "demo1":
+                return new Result(Code.ASSEMBLE_SEARCH_OK, "success");
+            case "demo2":
+                return new Result(Code.GAPFILL_SEARCH_OK, "success");
+            case "demo3":
+                return new Result(Code.TELO_SEARCH_OK, "success");
+            case "demo4":
                 return new Result(Code.CENTRO_SEARCH_OK, "success");
         }
         return null;
